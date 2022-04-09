203dec9e7773f166b7c76aa8fd8b93dbf9078b57
Haochen Li <lihaochen.9@bytedance.com>
2022-04-09 20:50:46 +0800

    feat: global scss vars introduced

---
+++ src/styles/variables/index.js
@@ -0,0 +1,11 @@
+/**
+ * @description: To insert global scss variables into the project
+ * @param {void}
+ * @return {string}
+ */
+export function scssVars() {
+    return `
+    $--color-primary:#76553f;
+    $--color-secondary:#C17754;
+    `;
+}
--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import vue from '@vitejs/plugin-vue';
 import AutoImport from 'unplugin-auto-import/vite';
 import Components from 'unplugin-vue-components/vite';
 import { ElementPlusResolver } from 'unplugin-vue-components/resolvers';
+import { scssVars } from './src/styles/variables';
 
 export default defineConfig({
     resolve: {
@@ -19,7 +20,8 @@ export default defineConfig({
         preprocessorOptions: {
             scss: {
                 // 自定义的主题色
-                additionalData: `@use "@/styles/element/index.scss" as *;`,
+                additionalData:
+                    `@use "@/styles/element/index.scss" as *;` + scssVars(),
             },
         },
     },
